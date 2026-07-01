236a32b2d4af470ffeed49fc145da371e501424f
lyritzis286 <lyritzis286@gmail.com>
2026-07-01 20:31:28 +0300

    add teacher-course relationship and override equals/hashCode methods

--- src/main/java/gr/aueb/cf/model/Teacher.java
+++ src/main/java/gr/aueb/cf/model/Teacher.java
@@ -1,9 +1,14 @@
 package gr.aueb.cf.model;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "teachers")
@@ -28,8 +33,31 @@ public class Teacher {
         this.firstname = firstname;
     }
 
+    @Getter(AccessLevel.PACKAGE)
+    @Setter(AccessLevel.NONE)
+    @OneToMany(mappedBy = "teacher", fetch = FetchType.LAZY)
+    private Set<Course> courses = new HashSet<>();
+
+    public Set<Course> getAllCourses() {
+        return Collections.unmodifiableSet(courses);
+    }
+
+    //Helper Methods
+    public void addCourse(Course course) {
+        courses.add(course);
+        course.setTeacher(this);
+    }
+
+    public void removeCourse(Course course) {
+        courses.remove(course);
+        course.setTeacher(null);
+    }
+
+
     @Override
     public String toString() {
         return String.format("%d %s, %s",  id, firstname, lastname);
     }
+
+
 }
