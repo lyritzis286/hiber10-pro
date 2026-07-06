39613a2540350b316631c79e75261033d6df3e57
lyritzis286 <lyritzis286@gmail.com>
2026-07-06 20:48:23 +0300

    add queries to select teachers by course title and count their courses

--- src/main/java/gr/aueb/cf/Main.java
+++ src/main/java/gr/aueb/cf/Main.java
@@ -32,13 +32,35 @@ public class Main {
 //                    em.createQuery(query2, Course.class).getResultList();
 //            courses.forEach(System.out::println);
 
-            //Select all courses of a teacher
-            String query3 = "SELECT c FROM Course c WHERE c.teacher" +
-                    ".lastname = 'Καπέτης'";
-            List<Course> kapetisCourses =
-                    em.createQuery(query3, Course.class).getResultList();
-            kapetisCourses.forEach(System.out::println);
+            //Select all courses of a teacher SQL Injection free
+//            String query4 = "SELECT c FROM Course c WHERE c.teacher" +
+//                    ".lastname = :lastname";
+//            List<Course> courses4 =
+//                    em.createQuery(query4, Course.class)
+//                            .setParameter("lastname", "Καπέτης")
+//                            .getResultList();
+//            courses4.forEach(System.out::println);
 
+            //Select teacher that teach Java
+            String query5 = "SELECT t FROM Teacher t " +
+                    "JOIN t.courses c WHERE c.title = :title";
+            List<Teacher> teachers5 = em.createQuery(query5, Teacher.class)
+                    .setParameter("title", "Java" )
+                    .getResultList();
+            teachers5.forEach(System.out::println);
+
+            //Select teacher lastname and the count of his courses
+            String query6 = "SELECT t.id, t.lastname, COUNT(c)" +
+                    "FROM Teacher t JOIN t.courses c GROUP BY t.id";
+            List<Object[]> teachers6 = em.createQuery(query6, Object[].class)
+                            .getResultList();
+
+            for(Object[] teacher : teachers6){
+                Long id = (Long)teacher[0];
+                String lastname = (String)teacher[1];
+                Long count = (Long)teacher[2];
+                System.out.println(id + " " + lastname + " " + count);
+            }
 
 
 
